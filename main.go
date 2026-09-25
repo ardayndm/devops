ca880ed6fe581964f0850ffc8d52c2cdb08859a5
ardayndm <138328104+ardayndm@users.noreply.github.com>
2026-09-25 23:07:23 +0300

    Go result code percentage added , now %65 succes %35 return error. CI updated for learning.

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"os"
+)
+
 func Add(a, b int) int {
 	return a + b
 }
 
 func main() {
 
+	if rand.Intn(100) < 65 {
+		fmt.Println("Application started successfully (%65!)")
+		return
+	}
+
+	fmt.Println("Application failed to start (%35!)")
+	os.Exit(1)
 }
