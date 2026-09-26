9e7005b47d5375e9f69a349590594510983a31b4
ardayndm <138328104+ardayndm@users.noreply.github.com>
2026-09-26 19:10:33 +0300

    Retry mechanicsm added

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ func Add(a, b int) int {
 
 func main() {
 
-	if rand.Intn(100) < 60 {
-		fmt.Println("Application started successfully (%60!)")
+	if rand.Intn(100) < 50 {
+		fmt.Println("Application started successfully (%50!)")
 		return
 	}
 
-	fmt.Println("Application failed to start (%40!)")
+	fmt.Println("Application failed to start (%50!)")
 	os.Exit(1)
 }
