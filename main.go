8bdc216fb58cbd7726c2ec7ccf028d2dd48b3c37
ardayndm <138328104+ardayndm@users.noreply.github.com>
2026-09-26 18:46:07 +0300

    App success percentage updated (50/50 to 60/40)

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ func Add(a, b int) int {
 
 func main() {
 
-	if rand.Intn(100) < 50 {
-		fmt.Println("Application started successfully (%50!)")
+	if rand.Intn(100) < 60 {
+		fmt.Println("Application started successfully (%60!)")
 		return
 	}
 
-	fmt.Println("Application failed to start (%50!)")
+	fmt.Println("Application failed to start (%40!)")
 	os.Exit(1)
 }
